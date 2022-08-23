b7f7e212592b2a4668e0f36aa18281786faf88cd
NicolasB2H <nicolas_omar97@hotmail.com>
2022-08-22 22:12:09 -0500

    Test1

--- src/main/java/com/modelo/entidades/Mensaje.java
+++ src/main/java/com/modelo/entidades/Mensaje.java
@@ -24,5 +24,23 @@ public class Mensaje implements Serializable {
 	public Mensaje() {
 		super();
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	
    
 }
